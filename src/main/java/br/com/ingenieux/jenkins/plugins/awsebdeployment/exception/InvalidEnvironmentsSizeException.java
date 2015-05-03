28143b0dcfccc60d6573299ffdf8f5026e94cfc4
Rafal Janicki <rafal@kontakt.io>
2015-05-03 19:44:39 +0200

    Finished zero downtime

--- src/main/java/br/com/ingenieux/jenkins/plugins/awsebdeployment/exception/InvalidEnvironmentsSizeException.java
+++ src/main/java/br/com/ingenieux/jenkins/plugins/awsebdeployment/exception/InvalidEnvironmentsSizeException.java
@@ -2,11 +2,20 @@ package br.com.ingenieux.jenkins.plugins.awsebdeployment.exception;
 
 public class InvalidEnvironmentsSizeException extends Exception {
 
-    public InvalidEnvironmentsSizeException() {
+    private final String applicationName;
+    private final String environmentName;
+
+    public InvalidEnvironmentsSizeException(String applicationName, String environmentName) {
         super();
+        this.applicationName = applicationName;
+        this.environmentName = environmentName;
     }
 
-    public InvalidEnvironmentsSizeException(String message) {
-        super(message);
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public String getEnvironmentName() {
+        return environmentName;
     }
 }
